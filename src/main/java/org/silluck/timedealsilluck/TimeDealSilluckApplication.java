b8ad14f07458092bce4e943a9d6415cede7a32ae
hanchanhee <hanssang6301@gamil.com>
2024-08-08 14:17:46 +0900

    Initial commit - 프로젝트 초기 설정
    - 의존성 추가 및 db 연결
      - jwt 의존성 추가
      - mysql 연결

---
+++ src/main/java/org/silluck/timedealsilluck/TimeDealSilluckApplication.java
@@ -0,0 +1,13 @@
+package org.silluck.timedealsilluck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDealSilluckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeDealSilluckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/silluck/timedealsilluck/TimeDealSilluckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.silluck.timedealsilluck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeDealSilluckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
