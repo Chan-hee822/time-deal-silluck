f52ece96f51737feb13d0b5012ffe89de50b6902
hanchanhee <hanssang6301@gamil.com>
2024-08-09 22:06:42 +0900

    [BE] feat: 고객에 대한 SignUp service 구현
    - entity에서 자주 사용하는 필드 baseEntity로 분리
    - 스프릉부트 테스트로 signup 테스트 코드 작성
    - 이메일 send test controller 삭제

---
+++ user-api/src/test/java/org/silluck/user/service/SignUpCustomerServiceTest.java
@@ -0,0 +1,35 @@
+package org.silluck.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.silluck.user.domain.SignUpForm;
+import org.silluck.user.domain.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@TestPropertySource(properties = "mailgun.key=test_key")
+@Transactional  // 테스트 후 데이터 롤백
+class SignUpCustomerServiceTest {
+
+    @Autowired
+    private SignUpCustomerService signUpCustomerService;
+
+    @Test
+    void signUp() {
+
+        SignUpForm form = SignUpForm.builder()
+                .email("email123@test.com")
+                .nickname("nickname")
+                .phone("01012341234")
+                .address("seoul")
+                .password("1111")
+                .build();
+
+        Customer c = signUpCustomerService.signUp(form);
+        assertEquals("email123@test.com", c.getEmail());
+    }
+}
