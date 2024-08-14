b2ebeb9aa734b7013ded46d7c9ac946ecc200480
hanchanhee <hanssang6301@gamil.com>
2024-08-15 06:07:11 +0900

    [BE] feat: MSA 1단계 Eureka 및 api-gateway server 구축
    - Eureka로 Client-Side Discovery를 구현하여 각 서비스들의 정보를 관리하는 체계 구축
    - 인증을 처리하기 위해 Spring Cloud Gateway로 api-gateway 서버 구축 및 초기 설정

--- user-api/src/main/java/org/silluck/user/UserApiApplication.java
+++ user-api/src/main/java/org/silluck/user/UserApiApplication.java
@@ -2,6 +2,7 @@ package org.silluck.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class UserApiApplication {
 
     public static void main(String[] args) {
